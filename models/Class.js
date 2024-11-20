5e7c5a630a31b8713825991885103fb9f710273d
V1NW0G <vincentwongck110@gmail.com>
2024-11-20 16:14:53 +0800

    fix/course desciprtion validation

--- models/Class.js
+++ models/Class.js
@@ -5,7 +5,7 @@ const classSchema = new mongoose.Schema({
     courseid: { type: Number, required: true },  // Course this class belongs to
     date: { type: String, required: true },
     teacher: { type: String, required: true },
-    comment: { type: String, required: true }
+    comment: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Class', classSchema);
